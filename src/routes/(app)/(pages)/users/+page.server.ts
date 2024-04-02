3fe04cef8de70dfc9ed3f34eb96302a43f0e8e59
theomaro <theo.pombe@gmail.com>
2024-04-02 15:13:17 +0300

    add limit and page to a request

--- src/routes/(app)/(pages)/users/+page.server.ts
+++ src/routes/(app)/(pages)/users/+page.server.ts
@@ -2,13 +2,18 @@ import { API_URL } from "$env/static/private";
 import { fail } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ fetch, cookies, parent }) => {
-  await parent()
-  
+export const load: PageServerLoad = async ({ fetch, url, cookies, parent }) => {
+  await parent();
+
+  const limit = url.searchParams.get("limit") ?? "";
+  const page = url.searchParams.get("page") ?? "";
+
   const res = await fetch(`${API_URL}/users`, {
     method: "POST",
     body: JSON.stringify({
       token: cookies.get("session"),
+      limit: parseInt(limit) || 10,
+      page: parseInt(page) || 1,
     }),
     headers: { "content-type": "application/json" },
   }).then((res) => res.json());
